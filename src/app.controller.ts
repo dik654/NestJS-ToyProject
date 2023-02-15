4491c1b2cf4cc12144c4d5504fa9d5916354d803
daeikkim <12377dl@gmail.com>
2023-02-15 10:28:34 +0900

    feature: ioredis 패키지 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject } from '@nestjs/common';
+import { InjectRedis, Redis} from '@nestjs-modules/ioredis';
+import { Cache } from 'cache-manager'
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: CacheQueryOptions,
+    @InjectRedis() private readonly redis: Redis,
+    private readonly appService: AppService
+  ) {}
 
-  @Get()
-  getHello(): string {
+  @Get() 
+  getHello():  string {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import 'dotenv/config';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RedisService } from './app.service';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [],
+  imports: [
+    RedisModule.forRoot({
+      config: {
+        url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+        password: process.env.REDIS_PASSWORD, 
+      },
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [RedisService],
 })
 export class AppModule {}
